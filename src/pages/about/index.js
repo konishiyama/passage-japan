81b5d5fab8ef78b42a262fa6daa1b09b6f8d5689
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2020-11-18 11:17:28 +0900

    Message_Added

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -15,9 +15,12 @@ const Farm = props => {
           style={{
             margin: `0 auto`,
             maxWidth: 650,
-            padding: `0em 2rem`,
+            padding: `2rem 2rem 0em`,
           }}
         >
+          <Fade duration={2000}>
+            <Index>農園について</Index>
+          </Fade>
           <Fade duration={2000}>
             <Pic>
               <img
@@ -27,11 +30,13 @@ const Farm = props => {
             </Pic>
           </Fade>
           <Fade duration={2000}>
-            <Index>農園について</Index>
             <P>
               自然豊かな栃木県日光市にて紫芋を中心とした有機農業を営んでいます。
               家族経営のこじんまりとした農園ですが、その分、一つ一つの作物を緻密に研究し、大切に育て、少しでも安全で美味しい野菜をお客様に直接お届けしたいと考えています。
             </P>
+          </Fade>
+          <Fade duration={2000}>
+            <Index>目標について</Index>
           </Fade>
           <Fade duration={2000}>
             <Pic>
@@ -42,7 +47,6 @@ const Farm = props => {
             </Pic>
           </Fade>
           <Fade duration={2000}>
-            <Index>目標について</Index>
             <P>
               当農園の目標は、安全・安心・美味しい野菜をお客様に提供し、Made In
               Japan・Japanブランドの野菜の素晴らしさ・価値を再認識していただくことです。
@@ -50,19 +54,27 @@ const Farm = props => {
             </P>
           </Fade>
           <Fade duration={2000}>
+            <Index>代表メッセージ</Index>
+          </Fade>
+          <Fade duration={2000}>
             <Pic>
               <img
-                src="https://firebasestorage.googleapis.com/v0/b/shohei-s-webapp-with-gatsby.appspot.com/o/site_default_images%2Fprofile_pic_height_optimized.jpg?alt=media&token=57bae787-a0e7-4735-a436-347e5f6123b4"
+                src="https://firebasestorage.googleapis.com/v0/b/shohei-s-webapp-with-gatsby.appspot.com/o/site_default_images%2Fyamaguchi.jpg?alt=media&token=77a966c9-1ca6-4ec6-a5c9-4162aa6c334b"
                 alt="coverImg"
               ></img>
             </Pic>
           </Fade>
           <Fade duration={2000}>
-            <Index>代表メッセージ</Index>
-            <P>調整中</P>
+            <P1>
+              未来の子供たちの「食」を守りたい。
+              そんな途方もない夢に憧れて農業を開始し、数年が経ちました。
+              スマート農業等による機械化や効率化が叫ばれる時代に、自信を持って食卓に並べられるような素材を確実に届けられるよう、手摘みにこだわっております（実際は、手摘みでしか経験できない収穫時の感動を手放したくないだけなのかもしれません。笑）。
+              人口減少やグローバル化の影響で野菜に求められる「質」は年々上がっているなかでも、変わらぬ価値を提供できるよう全力を努めて参りますので、よろしくお願いいたします。
+            </P1>
+            <P2>2020.1.1</P2>
+            <P3>山口農園代表　山口 吉一</P3>
           </Fade>
         </div>
-        <br></br>
       </section>
     </>
   )
@@ -71,6 +83,7 @@ const Farm = props => {
 const Pic = styled.div`
   position: relative;
   margin-bottom: 1rem;
+  margin-top: 1rem;
   text-align: center;
   display: inline-block;
 
@@ -94,7 +107,8 @@ const Index = styled.p`
     "Roboto Slab", Garamond, "Times New Roman", "HGS明朝E", "ＭＳ Ｐ明朝",
     "MS PMincho", serif;
   line-height: 1.5;
-  margin-bottom: 1rem;
+  margin: 0 auto 1rem;
+  text-align: center;
 `
 
 const P = styled.p`
@@ -105,7 +119,40 @@ const P = styled.p`
     "Roboto Slab", Garamond, "Times New Roman", "HGS明朝E", "ＭＳ Ｐ明朝",
     "MS PMincho", serif;
   line-height: 1.5;
-  margin-bottom: 3rem;
+  margin-bottom: 4rem;
+`
+
+const P1 = styled.p`
+  font-size: 14px;
+  color: #222;
+  font-family: "游明朝", "Yu Mincho", "游明朝体", "YuMincho",
+    "ヒラギノ明朝 Pro W3", "Hiragino Mincho Pro", "HiraMinProN-W3",
+    "Roboto Slab", Garamond, "Times New Roman", "HGS明朝E", "ＭＳ Ｐ明朝",
+    "MS PMincho", serif;
+  line-height: 1.5;
+  margin-bottom: 1rem;
+`
+
+const P2 = styled.p`
+  font-size: 14px;
+  color: #222;
+  font-family: "游明朝", "Yu Mincho", "游明朝体", "YuMincho",
+    "ヒラギノ明朝 Pro W3", "Hiragino Mincho Pro", "HiraMinProN-W3",
+    "Roboto Slab", Garamond, "Times New Roman", "HGS明朝E", "ＭＳ Ｐ明朝",
+    "MS PMincho", serif;
+  line-height: 1.5;
+  margin-top: 1rem;
+  margin-bottom: 0;
+`
+
+const P3 = styled.p`
+  font-size: 14px;
+  color: #222;
+  font-family: "游明朝", "Yu Mincho", "游明朝体", "YuMincho",
+    "ヒラギノ明朝 Pro W3", "Hiragino Mincho Pro", "HiraMinProN-W3",
+    "Roboto Slab", Garamond, "Times New Roman", "HGS明朝E", "ＭＳ Ｐ明朝",
+    "MS PMincho", serif;
+  line-height: 1.5;
 `
 
 export default Farm
