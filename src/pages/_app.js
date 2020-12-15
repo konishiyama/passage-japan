e03bc5b69e19e0d245e01d4fbde5f03a59f7ebeb
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2020-12-15 16:33:19 +0900

    Google_Analytics_Added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,21 @@ import "slick-carousel/slick/slick.css"
 import Layout from "../components/layout"
 import "../components/layout.css"
 import "../../public/fonts/style.css"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as gtag from "../lib/gtag"
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
   return (
     <Layout>
       <Component {...pageProps} />
